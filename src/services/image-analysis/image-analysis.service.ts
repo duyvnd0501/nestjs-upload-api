61fc773e6bd1a7beb7c5844e84d9625650f43034
voduy <vonguyenduyduy@gmail.com>
2025-03-28 08:49:54 +0700

    update Simulate mock api

--- src/services/image-analysis/image-analysis.service.ts
+++ src/services/image-analysis/image-analysis.service.ts
@@ -1,18 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import fs from 'fs';
 
 @Injectable()
 export class ImageAnalysisService {
   // Simulate an image analysis API (OpenAI-like)
   async analyzeBrand(imagePath: string): Promise<any> {
-    // Simulate a delay for the mock analysis
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        // Mock result: Assume brand detected with exposure time of random seconds
-        resolve({
-          exposureTime: Math.floor(Math.random() * 100), // Exposure time in seconds
-          detectedLocation: 'center', // Where it was visible
-        });
-      }, 100);
-    });
+    try {
+      const fileContent = fs.readFileSync(imagePath);
+      // Simulating an image file content analysis request to a mock OpenAI API.
+      // const response = await axios.post('https://api.mockopenai.com/analyze', {
+      //   image: fileContent,
+      // });
+      // Simulate a delay for the mock analysis
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          // Mock result: Assume brand detected with exposure time of random seconds
+          resolve({
+            exposureTime: Math.floor(Math.random() * 100), // Exposure time in seconds
+            detectedLocation: 'center', // Where it was visible
+          });
+        }, 100);
+      });
+    } catch (e) {
+      console.log("Error analyzeBrand", e);
+      throw new Error('Failed to process video frames');
+    }
   }
 }
